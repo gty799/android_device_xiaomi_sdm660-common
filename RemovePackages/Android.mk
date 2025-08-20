6dcd559d9a797e526a9660542a9d9a8f27ff606a
gty799 <59225496+gty799@users.noreply.github.com>
2025-08-20 18:41:13 +0300

    Lavender : Update Remove Packages

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -7,25 +7,13 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
     AudioFX \
     Aperture \
-    Chrome \
-    Chrome-stub \
-    AndroidAutoStubPrebuilt \
-    PrebuiltGmail \
-    CalendarGooglePrebuilt \
     DevicePolicyPrebuilt-v10306480 \
     DevicePersonalizationPrebuiltPixel2024-U.32_V.7_playstore_aiai_20240725.00_RC08 \
     DevicePersonalizationPrebuiltPixel2024-bfinal_aiai_20250217.00_RC08 \
-    GoogleTTS \
-    GoogleRestorePrebuilt-v636048 \
     Etar \
-    ExactCalculator \
-    EmergencyInfo \
     FM2 \
     Jelly \
     LiveWallpapersPicker \
-    Music \
-    Maps \
-    Photos \
     SoundAmplifierPrebuilt_v4.7.638126989 \
     SwitchAccessPrebuilt_1.16.0.726766860 \
     TurboPrebuilt \
@@ -36,11 +24,8 @@ LOCAL_OVERRIDES_PACKAGES := \
     Stk \
     Traceur \
     Twelve \
-    WellbeingPrebuilt \
-    Velvet \
     MatLog \
-    QuickAccessWallet \
-    ViaBrowser
+    QuickAccessWallet 
 
 
 LOCAL_UNINSTALLABLE_MODULE := true
