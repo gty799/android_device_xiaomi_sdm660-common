cb032e477e0bd42f3a2a0fcf8f7e8a00b5b48f42
mitmac29 <miteshjandj@gmail.com>
2025-08-20 15:34:54 +0000

    Lavender : Add remove packages
    This reverts commit 6fc2c3f7c2e1b49519e70504c2f748a93da1414c.

---
+++ RemovePackages/Android.mk
@@ -0,0 +1,49 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    AudioFX \
+    Aperture \
+    Chrome \
+    Chrome-stub \
+    AndroidAutoStubPrebuilt \
+    PrebuiltGmail \
+    CalendarGooglePrebuilt \
+    DevicePolicyPrebuilt-v10306480 \
+    DevicePersonalizationPrebuiltPixel2024-U.32_V.7_playstore_aiai_20240725.00_RC08 \
+    DevicePersonalizationPrebuiltPixel2024-bfinal_aiai_20250217.00_RC08 \
+    GoogleTTS \
+    GoogleRestorePrebuilt-v636048 \
+    Etar \
+    ExactCalculator \
+    EmergencyInfo \
+    FM2 \
+    Jelly \
+    LiveWallpapersPicker \
+    Music \
+    Maps \
+    Photos \
+    SoundAmplifierPrebuilt_v4.7.638126989 \
+    SwitchAccessPrebuilt_1.16.0.726766860 \
+    TurboPrebuilt \
+    SecureElement \
+    ScribePrebuilt_v8.2.717788270 \
+    SafetyHubSuwPrebuilt \
+    Seedvault \
+    Stk \
+    Traceur \
+    Twelve \
+    WellbeingPrebuilt \
+    Velvet \
+    MatLog \
+    QuickAccessWallet \
+    ViaBrowser
+
+
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
