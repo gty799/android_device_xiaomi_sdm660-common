4b06c68e45ecaabcdef0d377168234cb27d467a7
gty799 <59225496+gty799@users.noreply.github.com>
2025-08-21 09:18:30 +0300

    Lavender: Update remove packages

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -11,6 +11,7 @@ LOCAL_OVERRIDES_PACKAGES := \
     DevicePersonalizationPrebuiltPixel2024-U.32_V.7_playstore_aiai_20240725.00_RC08 \
     DevicePersonalizationPrebuiltPixel2024-bfinal_aiai_20250217.00_RC08 \
     Etar \
+    FMRadio \
     FM2 \
     Jelly \
     LiveWallpapersPicker \
