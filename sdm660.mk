3c5073d06bf000515d3fd41dcb7ba55b0f3e5c7d
xetob <xetob1122@gmail.com>
2025-08-26 19:09:47 +0000

    Reapply "Lavender : Delete ViperFX add Dolby, Remove Packages"
    This reverts commit 75df41f1a6853b6137ff22474f337fdb8a81d177.

--- sdm660.mk
+++ sdm660.mk
@@ -4,17 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Remove packages
-<<<<<<< HEAD
 PRODUCT_PACKAGES += \
     RemovePackages
 
 $(call inherit-product, hardware/dolby/dolby.mk)
 # $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
-=======
-# PRODUCT_PACKAGES += \ RemovePackages
-
-$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
->>>>>>> parent of a321080c (Lavender : Delete ViperFX add Dolby, Remove Packages)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
